5d35a5b4a5fee1c67ff43fbaa12edeb0ac686157
Rafael dela Cruz <rafael.delacruz@republisys.com>
2017-08-04 09:54:22 +0800

    address bar icon url

--- public/themes/default/layouts/default.blade.php
+++ public/themes/default/layouts/default.blade.php
@@ -2,11 +2,13 @@
 <html>
     <head>
         <title>{!! Theme::get('title') !!}</title>
+        <link rel="SHORTCUT ICON" href="../assets/img/rafael.jpg">
         <meta charset="utf-8">
         <meta name="keywords" content="{!! Theme::get('keywords') !!}">
         <meta name="description" content="{!! Theme::get('description') !!}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         {!! Theme::asset()->styles() !!}
+
     </head>
     <body>
 <!--        {!! Theme::partial('header') !!}-->
